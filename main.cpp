9b43bc1f337e2d439312ad7d570dc33f0a37f4d5
i-dont-have-any-good-names <67664504+i-dont-have-any-good-names@users.noreply.github.com>
2023-01-12 16:46:22 +0000

    Level ups

--- main.cpp
+++ main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
+#include <string>
 
-using std::cout;
+using std::cout; using std::string; using std::cin;
 
 class Player {
 public:
+    int coal = 20;
+    int iron = 30;
+
+    string call;
+    string username;
     int level;
     int xp;
-    void stats() {
-    if(xp >= 100) {
-        xp -= 100;
-        level += 1;
+    int levelUp;
+    void Levels() {
+        if(xp >= 100) {
+            xp -= 100;
+            level += 1;
+        }
     }
-        cout << "Your level is - "<< level << "\n";
+
+    void LevelsUp() {
+        if(call == "coal") {
+            xp += coal;
+            cout << "+ " << coal << "\n";
+        }
+        else if(call == "iron") {
+            xp += iron;
+            cout << "+ " << coal << "\n";
+        }
+        else {
+            cout << "+ 0";
+        }
+    }
+
+    void Stats() {
+        cout << "Username - "<< username << "\n";
+        cout << "Level - "<< level << "\n";
+        cout << "XP - "<< xp << "\n";
     }
 };
 
 int main() {
     Player player1;
+    player1.username = "Car";
     player1.level = 10;
-    player1.xp = 112;
-    player1.stats();
+    player1.xp = 50;
+    player1.Levels();
+    cout << "Mine: ";
+    cin >> player1.call;
+    player1.LevelsUp();
+    player1.Stats();
     return 0;
 }
